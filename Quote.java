d175c8d9396a9de7a53d08f98ea4d027db0f124e
Isabel Solano <60373842+MaIsabelSolano@users.noreply.github.com>
2021-03-13 17:22:04 -0600

    Add files via upload
    Clase quote

---
+++ Quote.java
@@ -0,0 +1,12 @@
+class Quote{
+
+	/**
+	 *
+	 * @param impresion String de datos que no se desea evaluar
+	 * @return el mismo string
+	 */
+	public String DevolverQuote(String impresion){
+		System.out.println(impresion);
+		return impresion;
+	}
+}
